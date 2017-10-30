6878dfcd207ecf6d6ddb14a102d2d1e1b759ee28
Aris <arisbarbuco@gmail.com>
2017-10-30 22:17:32 +0800

    some pages ui developed

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -14,15 +14,3 @@ $('.add-quantity').click(function(){
 $('.minus-quantity').click(function(){
     $('.pagination-circle > .pagination-active').prev('li').find('a').trigger('click');
 });
-
-$(document).ready(function() {
-    $(".limit-char-45").on('keydown', function(e) {
-        var words = $(this).val().length;
-        if (words <= 10) {
-            $('.word-count').text(words);
-            $('.word-left').text(10-words);
-        }else{
-            if (e.which !== 8) e.preventDefault();
-        }
-    });
-});
